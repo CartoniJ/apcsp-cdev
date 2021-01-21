cf063b445a6062ba73ffe023d58653c52bbb853e
James Cartoni <cartonij2181@my.sduhsd.net>
2021-01-21 23:07:30 +0000

    Updated proj

--- proj/student.c
+++ proj/student.c
@@ -1,4 +1,4 @@
-//
+ //
 // student.c
 //
 
@@ -23,16 +23,13 @@ void createStudent(char* fname, char* lname, int age, int id)
 {
   Student* st = (Student*)malloc(sizeof(Student));
 
-
   st->firstName = (char*)malloc((strlen(fname)+1)*sizeof(char));
   st->lastName = (char*)malloc((strlen(lname)+1)*sizeof(char));
 
   strcpy(st->firstName, fname);
   strcpy(st->lastName, lname);
-
   st->age = age;
   st->id = id;
-
 
   students[numStudents] = st;
   numStudents++;
@@ -49,35 +46,34 @@ void deleteStudent(Student* student)
 
 void deleteStudents()
 {
-  int it;
-  for (it = 1; it < 100; ++it) {
-    deleteStudent(students[it]);
-    students[it] = 0;
+  int i;
+  for (i = 0; i < numStudents; i++) {
+    deleteStudent(students[i]);
+    students[i] = 0;
   }
-
   numStudents = 0;
 }
 
 
 void saveStudents(int key)
 {
-  FILE *file;
-  file = fopen("studentdata.txt", "w");
-  if (file) {
-    int it;
+  FILE* fp;
+
+  fp = fopen(STUFILE, "w");
+  if(fp) {
     char buff[256];
-    for (it = 1; it < 100; ++it) {
-      Student* st = students[it];
+    for (int i = 0; i < numStudents; i++) {
+      Student* st = students[i];
       sprintf(buff, "%s %s %d %ld", st->firstName, st->lastName, st->age, st->id);
-      
+
       if (key != 0) {
         caesarEncrypt(buff, key);
       }
-      
-      fprintf(file, "%s\n", buff);
+
+      fprintf(fp, "%s\n", buff);
     }
-    
-    fclose(file);
+
+    fclose(fp);
   }
 }
 
@@ -87,16 +83,17 @@ void loadStudents(int key)
   if (numStudents > 0) {
     deleteStudents();
   }
-  
-  FILE *file;
-  file = fopen("studentdata.txt", "r");
-  if(file) {
+
+  FILE* fp;
+
+  fp = fopen(STUFILE, "r");
+  if(fp) {
     char b1[256];
     char b2[256];
     char b3[256];
     char b4[256];
 
-    int match = fscanf(file, "%s %s %s %s", b1, b2, b3, b4);
+    int match = fscanf(fp, "%s %s %s %s", b1, b2, b3, b4);
     if (match == 4) {
       if (key != 0) {
         caesarDecrypt(b1, key);
@@ -107,9 +104,8 @@ void loadStudents(int key)
 
       int age;
       long id;
-
       sscanf(b3, "%d", &age);
-      sscanf(b3, "%ld", &id);
+      sscanf(b4, "%ld", &id);
 
       createStudent(b1, b2, age, id);
     }
@@ -121,7 +117,7 @@ void printStudent(Student* student)
 {
   printf("  Student: %s %s\n", student->firstName, student->lastName);
   printf("    age: %d\n", student->age);
-  printf("    id: %ld\n", student->id); 
+  printf("    id: %ld\n", student->id);
 }
 
 
